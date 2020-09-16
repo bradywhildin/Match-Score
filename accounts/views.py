a165200959f533508b72fdff8b1a0ee2ac8d4c7f
Brady Whildin <bradwhil@gmail.com>
2020-09-16 18:17:55 -0400

    Modified get users endpoint to not include current user in response

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,8 @@ from django.contrib.auth.models import User
 from .serializers import UserSerializer
 from rest_framework import generics
 from rest_framework.permissions import AllowAny
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 class UserListCreate(generics.ListCreateAPIView):
     permission_classes = [AllowAny,]
@@ -9,3 +11,7 @@ class UserListCreate(generics.ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+    def list(self, request):
+        queryset = self.get_queryset().exclude(id=request.user.id)
+        serializer = UserSerializer(queryset, many=True)
+        return Response(serializer.data)
