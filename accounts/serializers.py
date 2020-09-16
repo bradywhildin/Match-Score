7034c0d37bdcb5cf0c85f84dd5445f99998e72d4
Brady Whildin <bradwhil@gmail.com>
2020-09-16 11:59:05 -0400

    Added first and last name to user object

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -8,7 +8,7 @@ class UserSerializer(serializers.ModelSerializer):
         write_only_fields = ('password')
 
     def create(self, validated_data):
-        user = User(username=validated_data['username'])
+        user = User(username=validated_data['username'], first_name=validated_data['first_name'], last_name=validated_data['last_name'])
         user.set_password(validated_data['password'])
         user.save()
         return user
